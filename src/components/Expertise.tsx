7047d30987670ecd92757864e8cc2808e57c6061
kenji.izon.cruses@apcas.ph.education <112867682+kendifyy@users.noreply.github.com>
2025-09-14 23:52:09 +0800

    Update portfolio

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -55,7 +55,7 @@ function Expertise() {
 
                 <div className="skill">
                     <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
+                    <h3>Developer Tools</h3>
                     <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
